532540a08317e84db0f360fd6ba93a024254bb98
Garyo <r.ga1ightning@gmail.com>
2025-08-10 14:15:10 +0900

    ♻️ Linksページのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,34 +1,42 @@
 import Image from "next/image";
 import styles from "@/components/Links.module.css";
 
+const ITEMS = [
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy now",
+  },
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Read our docs",
+  },
+  {
+    href: "https://vercel.com/garyos-projects/~/activity",
+    title: "My activity",
+  },
+  {
+    href: "https://vercel.com/garyos-projects",
+    title: "My projects",
+  },
+];
+
 export function Links() {
   return (
-    <>
-      <div className={styles.ctas}>
-        <a
-          className={styles.primary}
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            className={styles.logo}
-            src="/vercel.svg"
-            alt="Vercel logomark"
-            width={20}
-            height={20}
-          />
-          Deploy now
-        </a>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.secondary}
-        >
-          Read our docs
-        </a>
-      </div>
-    </>
+    <div className={styles.ctas}>
+      {ITEMS.map((item) => {
+        return (
+          <a key={item.href} href={item.href} className={styles.primary}>
+            <Image
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            {item.title}
+          </a>
+        );
+      })}
+    </div>
   );
 }
