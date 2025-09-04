40d75c846c342571f359081e9647c45129caac49
Garyo <r.ga1ightning@gmail.com>
2025-09-04 13:44:43 +0900

    ✨ useRouterの学習

--- src/hooks/useBgColor.jsx
+++ src/hooks/useBgColor.jsx
@@ -1,11 +1,19 @@
 import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
 
 export const useBgColor = () => {
+  const router = useRouter();
+
+  const bgColor = useMemo(() => {
+    return router.pathname === "/" ? "lightblue" : "lightgreen";
+  }, [router.pathname]);
+
   useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
+    document.body.style.backgroundColor = bgColor;
 
     return () => {
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [bgColor]);
 };
