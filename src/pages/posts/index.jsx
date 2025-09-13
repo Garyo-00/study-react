5b0e823ed23202aa8d135dc73dea203d50627729
Garyo <r.ga1ightning@gmail.com>
2025-09-13 14:19:22 +0900

    🚚 投稿系をpostsにパスを変更した

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import { Footer } from "../components/Footer";
-import { Posts } from "../components/Posts";
-import { Header } from "../components/Header";
+import { Footer } from "../../components/Footer";
+import { Posts } from "../../components/Posts";
+import { Header } from "../../components/Header";
 import { useState, useCallback, useEffect } from "react";
 
 const Home = (props) => {
