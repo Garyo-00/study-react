0bf07add3663a82137633620457d0f257d697f7d
Garyo <r.ga1ightning@gmail.com>
2025-09-13 14:32:00 +0900

    🔥 不必要なコードやファイルを削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,7 @@
 import "@/styles/globals.css";
-import { useCounter } from "../hooks/useCounter.jsx";
-import { useInputArray } from "../hooks/useInputArray.jsx";
-import { useBgColor } from "../hooks/useBgColor.jsx";
 
 const App = ({ Component, pageProps }) => {
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  useBgColor();
-
-  return <Component {...pageProps} {...counter} {...inputArray} />;
+  return <Component {...pageProps} />;
 };
 
 export default App;
