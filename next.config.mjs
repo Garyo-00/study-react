de39af59f2a0bf8f69e7462a40f6a94ed763cfa4
Garyo <r.ga1ightning@gmail.com>
2025-09-18 12:33:56 +0900

    💚 キャッシュのクリア

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default nextConfig; // ← これがESMの正しい書き方
